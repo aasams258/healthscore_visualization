69ab741e3f5497a4c3778b91986ad27938feba79
Arthur Sams <arthur@Arthurs-MacBook-Pro.local>
2018-08-09 18:10:28 -0700

    Fixed Schema for SQLITE3

--- restaurant_schema.sql
+++ restaurant_schema.sql
@@ -1,33 +1,37 @@
+-- sqlite3
+-- .read restaurant_schema, then check with .schema $TABLE_NAME
 CREATE TABLE la_restaurants (
-    id int primary key autoincrement,
-    name varchar(255),
-    name_alias varchar(255),
-    price varchar(255),
-    rating float(5,5),
+    id INTEGER PRIMARY KEY autoincrement,
+    name TEXT,
+    name_alias TEXT,
+    price TEXT,
+    rating REAL,
     review_count int,
-    img_url varchar(255),
-    yelp_url varchar(255),
-    address1 varchar(255),
-    address2 varchar(255),
-    address3 varchar(255),
-    city varchar(255),
-    zip_code varchar(255),
-    longitude float(15,15),
-    latitude float(15,15),
-    yelp_id varchar(255),
+    img_url TEXT,
+    yelp_url TEXT,
+    address1 TEXT,
+    address2 TEXT,
+    address3 TEXT,
+    city TEXT,
+    zip_code TEXT,
+    longitude REAL,
+    latitude REAL,
+    yelp_id TEXT,
 
     health_score int,
-    health_grade char(1),
-    biz_desc varchar(255),
-    biz_owner varchar(255),
-    inspection_date date,
+    health_grade TEXT,
+    biz_desc TEXT,
+    biz_owner TEXT,
+    inspection_date NUMERIC,
 
-    is_chain bit
+    is_chain INTEGER
 );
 
 CREATE TABLE restaurant_categories (
-    id int primary key autoincrement,
-    category_name varchar(255),
-    category_alias varchar(255),
-    restaurant_id int foreign key references la_restaurants.id
+    id INTEGER PRIMARY KEY autoincrement,
+    category_name TEXT,
+    category_alias TEXT,
+    restaurant_id INTEGER NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES la_restaurants(id)
 );
+
